67a48604d7c4186c0745488b4fde5729d22e7ad8
thefaizan1 <faizanshikalgar5@gmail.com>
2026-06-10 21:57:36 +0530

    Database Added

---
+++ database.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const mongoose = require('mongoose')
+const app=express()
+
+app.use(express.json())
+
+
+//database connection
+mongoose.connect("mongodb+srv://admin:admin@cluster0.nlj23qg.mongodb.net/userappnew")
+
+const User = mongoose.model('Users',{username:String,password:String})
+
+
+app.post("/register",async function(req,res){
+    const username = req.body.username;
+    const pwd=req.body.password
+
+    const existingUser = await User.findOne({username:username})
+    if(existingUser){
+        return res.status(400).send("User already Exists")
+    }
+    else{
+        const user =new User({
+        username:username,
+        password:pwd
+        })
+        user.save()
+        res.json({
+            msg:"User Saved successfully"
+        })
+    }
+
+})
+
+
+app.listen('3000',function(){
+    console.log("Server started on port 3000")
+})
+
+
+
